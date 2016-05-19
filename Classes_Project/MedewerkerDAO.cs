e7efa57fefbc7e400b5cb0d92fa13c10b5e5c4ee
Jasper <jasper.huizing@gmail.com>
2016-05-19 22:04:35 +0200

    nog een recommit, nu moet hij het echt doen.

---
+++ Classes_Project/MedewerkerDAO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Classes_Project
+{
+    public class MedewerkerDAO
+    {
+        SqlConnection conn;
+
+        public MedewerkerDAO()
+        {
+            conn = new SqlConnection(Helper.ConnectionString);
+            conn.Open();
+        }
+
+        public Medewerker GetByID(int id)
+        {
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM Medewerker WHERE id = {0} ", id), conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string voornaam = (string)reader["voornaam"];
+                string achternaam = (string)reader["achternaam"];
+                string rol = (string)reader["rol"];
+                int pincode = (int)reader["pincode"];
+
+                switch (rol)
+                {
+                    case "Keuken":
+                        Keuken keuken = new Keuken(id, pincode, voornaam, achternaam);
+                        return keuken;
+
+                    case "Bar":
+                        Bar bar = new Bar(id, pincode, voornaam, achternaam);
+                        return bar;
+
+                    case "Bediening":
+                        Bediening bediening = new Bediening(id, pincode, voornaam, achternaam);
+                        return bediening;
+
+                    case "Manager":
+                        Manager manager = new Manager(id, pincode, voornaam, achternaam);
+                        return manager;
+                }
+            }
+
+            return new Bediening(1, 1, "dit moet", "nog gefixt worden");
+        }
+        //public List<Medewerker> GetAll()
+        //{
+
+        //}
+
+        public Medewerker GetByPincode(string pincode)
+        {
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM Medewerker WHERE pincode = {0} ", pincode), conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string voornaam = (string)reader["voornaam"];
+                string achternaam = (string)reader["achternaam"];
+                string rol = (string)reader["rol"];
+                int id = (int)reader["id"];
+
+                switch (rol)
+                {
+                    case "Keuken":
+                        Keuken keuken = new Keuken(id, int.Parse(pincode), voornaam, achternaam);
+                        return keuken;
+
+                    case "Bar":
+                        Bar bar = new Bar(id, int.Parse(pincode), voornaam, achternaam);
+                        return bar;
+
+
+                    case "Bediening":
+                        Bediening bediening = new Bediening(id, int.Parse(pincode), voornaam, achternaam);
+                        return bediening;
+
+
+                    case "Manager":
+                        Manager manager = new Manager(id, int.Parse(pincode), voornaam, achternaam);
+                        return manager;
+                }
+            }
+            return new Bediening(1, 1, "dit moet", "nog gefixt worden");
+        }
+
+
+    }
+}
