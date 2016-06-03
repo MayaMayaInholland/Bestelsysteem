18e22825b6eb59f942d542d0a417e89aea373c96
Joep360 <alkmaar191@gmail.com>
2016-06-03 23:58:21 +0200

    Keuken 99,999%

--- Classes_Project/KeukenDAO.cs
+++ Classes_Project/KeukenDAO.cs
@@ -12,112 +12,102 @@ namespace Classes_Project
     {
         SqlConnection conn;
 
-        public void KeukenBestellingKlaar(int tafelID)
+        public void KeukenBestellingKlaar(int tafelnummer)
         {
             conn = new SqlConnection(Helper.ConnectionString);
             conn.Open();
-
-            SqlCommand cmd = new SqlCommand(string.Format("UPDATE Besteld_product SET status = 1 WHERE bestelling_id IN (SELECT id FROM Bestelling WHERE tafel_id = {0} AND product_id IN (SELECT id FROM Product WHERE categorie_id < 8))", tafelID), conn);
-            SqlDataReader reader = cmd.ExecuteReader();
+            SqlCommand cmd = new SqlCommand(string.Format("UPDATE Besteld_product SET status = 2 FROM Besteld_product, Product, Tafel, Bestelling WHERE Besteld_product.status = 1 AND product_id = Product.id AND tafel_id = Tafel.id AND Besteld_product.bestelling_id = Bestelling.id AND categorie_id < 8 AND nummer = {0} ", tafelnummer), conn);
+            // SqlDataReader reader = cmd.ExecuteReader();
             int resultaat = cmd.ExecuteNonQuery();
         }
 
 
-        public void KeukenBestellingNIETKlaar(int tafelID)
+        public void KeukenBestellingNIETKlaar(int tafelnummer)
         {
             conn = new SqlConnection(Helper.ConnectionString);
             conn.Open();
-
-            SqlCommand cmd = new SqlCommand(string.Format("UPDATE Besteld_product SET status = 2 WHERE bestelling_id IN (SELECT id FROM Bestelling WHERE tafel_id = {0} AND product_id IN (SELECT id FROM Product WHERE categorie_id < 8))", tafelID), conn);
-            SqlDataReader reader = cmd.ExecuteReader();
+            SqlCommand cmd = new SqlCommand(string.Format("UPDATE Besteld_product SET status = 1 FROM Besteld_product, Product, Tafel, Bestelling WHERE Besteld_product.status = 2 AND product_id = Product.id AND tafel_id = Tafel.id AND Besteld_product.bestelling_id = Bestelling.id AND categorie_id < 8 AND nummer = {0} ", tafelnummer), conn);
+            // SqlDataReader reader = cmd.ExecuteReader();
             int resultaat = cmd.ExecuteNonQuery();
         }
 
-        public List<Product> GetActiveBesteldeProducten()
+        public List<Besteld_product> GetActiveBesteldeProducten()
         {
             conn = new SqlConnection(Helper.ConnectionString);
             conn.Open();
 
             //De aangeklikte tafel geeft de tafel_id mee.
-            SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM Besteld_product INNER JOIN Product ON Besteld_product.product_id = Product.id WHERE Besteld_product.status = 1"), conn);
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT Besteld_product.id as Besteldproduct_id, bestelling_id, product_id, Besteld_product.status, aantal, Besteld_product.opmerking, categorie_id, omschrijving, nummer as Tafel_nummer FROM Besteld_product, Product, Tafel, Bestelling WHERE Besteld_product.status = 1 AND product_id = Product.id AND tafel_id = Tafel.id AND Besteld_product.bestelling_id = Bestelling.id AND categorie_id < 8 ORDER BY Tafel_nummer"), conn);
             SqlDataReader reader = cmd.ExecuteReader();
 
             //List waarin de al bestelde producten in komen.
-            List<Product> lijst_Activeroducten = new List<Product>();
+            List<Besteld_product> lijst_Activeroducten = new List<Besteld_product>();
 
             while (reader.Read())
             {
-                int BesteldProduct_id = (int)reader["id"];
-                int Bestelling_id = (int)reader["bestelling_id"];
+                int BesteldProduct_id = (int)reader["Besteldproduct_id"];
+                int Bestelling_id = (int)reader["Bestelling_id"];
                 int Product_id = (int)reader["product_id"];
                 int Status = (int)reader["status"];
                 int Aantal = (int)reader["aantal"];
-               // string Opmerkingen = "";
-                //if (reader["opmerkingen"] != DBNull.Value)
-                //{
-                //    Opmerkingen = (string)reader["opmerkingen"];
-                //}
-                int ProductId = (int)reader["id"];
+                string Opmerking = "";
+                if (reader["opmerking"] != DBNull.Value)
+                {
+                    Opmerking = (string)reader["opmerking"];
+                }
                 int Categorie_id = (int)reader["categorie_id"];
                 string Omschrijving = (string)reader["omschrijving"];
-                int Prijs = (int)reader["prijs"];
-                int Voorraad = (int)reader["voorraad"];
-                int Btw = (int)reader["btw"];
+                int Tafel_nummer = (int)reader["Tafel_nummer"];
 
 
                 //Overload van class product voor al bestaande bestellingen.... ( opgezet zodat code runt )
-                Product product = new Product(BesteldProduct_id, Categorie_id, Prijs, Voorraad, Btw, Omschrijving );
+                Besteld_product product = new Besteld_product(BesteldProduct_id, Bestelling_id, Product_id, Status, Aantal, Opmerking, Categorie_id, Omschrijving, Tafel_nummer);
 
                 lijst_Activeroducten.Add(product);
 
-                reader.Close();
-                conn.Close();
-                
+
+
             }
             reader.Close();
             conn.Close();
             return lijst_Activeroducten;
         }
 
-        public List<Product> GetKLAARBesteldeProducten()
+        public List<Besteld_product> GetKLAARBesteldeProducten()
         {
             conn = new SqlConnection(Helper.ConnectionString);
             conn.Open();
 
             //De aangeklikte tafel geeft de tafel_id mee.
-            SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM Besteld_product INNER JOIN Product ON Besteld_product.product_id = Product.id WHERE Besteld_product.status = 0"), conn);
+            SqlCommand cmd = new SqlCommand(string.Format("SELECT Besteld_product.id as Besteldproduct_id, bestelling_id, product_id, Besteld_product.status, aantal, Besteld_product.opmerking, categorie_id, omschrijving, nummer as Tafel_nummer FROM Besteld_product, Product, Tafel, Bestelling WHERE Besteld_product.status = 2 AND product_id = Product.id AND tafel_id = Tafel.id AND Besteld_product.bestelling_id = Bestelling.id AND categorie_id < 8 ORDER BY Tafel_nummer"), conn);
             SqlDataReader reader = cmd.ExecuteReader();
 
             //List waarin de al bestelde producten in komen.
-            List<Product> lijst_NietActiveroducten = new List<Product>();
+            List<Besteld_product> lijst_NietActiveroducten = new List<Besteld_product>();
 
             while (reader.Read())
             {
-                int BesteldProduct_id = (int)reader["Besteld_product.id"];
-                int Bestelling_id = (int)reader["bestelling_id"];
+                int BesteldProduct_id = (int)reader["Besteldproduct_id"];
+                int Bestelling_id = (int)reader["Bestelling_id"];
                 int Product_id = (int)reader["product_id"];
                 int Status = (int)reader["status"];
                 int Aantal = (int)reader["aantal"];
-                string Opmerkingen = "";
-                if (reader["opmerkingen"] != DBNull.Value)
+                string Opmerking = "";
+                if (reader["opmerking"] != DBNull.Value)
                 {
-                    Opmerkingen = (string)reader["opmerkingen"];
+                    Opmerking = (string)reader["opmerking"];
                 }
-                int ProductId = (int)reader["Product.id"];
                 int Categorie_id = (int)reader["categorie_id"];
                 string Omschrijving = (string)reader["omschrijving"];
-                int Prijs = (int)reader["prijs"];
-                int Voorraad = (int)reader["voorraad"];
-                int Btw = (int)reader["btw"];
+                int Tafel_nummer = (int)reader["Tafel_nummer"];
 
 
                 //Overload van class product voor al bestaande bestellingen.... ( opgezet zodat code runt )
-                Product product = new Product(BesteldProduct_id, Categorie_id, Prijs, Voorraad, Btw, Omschrijving);
+                Besteld_product product = new Besteld_product(BesteldProduct_id, Bestelling_id, Product_id, Status, Aantal, Opmerking, Categorie_id, Omschrijving, Tafel_nummer);
 
                 lijst_NietActiveroducten.Add(product);
 
-                reader.Close();
-                conn.Close();
+
 
             }
             reader.Close();
--- Classes_Project/KeukenOverzicht.cs
+++ Classes_Project/KeukenOverzicht.cs
@@ -16,11 +16,11 @@ namespace Classes_Project
         {
             InitializeComponent();
 
-            List<Product> Lijst_ActiveProducten = new List<Product>();
+            List<Besteld_product> Lijst_ActiveProducten = new List<Besteld_product>();
 
             KeukenDAO keukenDAO = new KeukenDAO();
             Lijst_ActiveProducten = keukenDAO.GetActiveBesteldeProducten();
-            
+
 
             lbl_IngelogdeMedewerker.Text = m.Voornaam;
 
@@ -30,118 +30,278 @@ namespace Classes_Project
 
             int y = 0;
             int yToe = 40;
-            int YLBL = 10;
+            int YLBL = 0;
+            int NieuweTafel = 0;
+            int Nieuwekleur = 1;
 
 
-            List<Product> lijst_NietActiveroducten = new List<Product>();
-            lijst_NietActiveroducten = keukenDAO.GetKLAARBesteldeProducten();
+            List<Besteld_product> lijst_NietActiveroducten = new List<Besteld_product>();
+
+            KeukenDAO keukenDAO2 = new KeukenDAO();
+            lijst_NietActiveroducten = keukenDAO2.GetKLAARBesteldeProducten();
 
             y = 0;
             yToe = 40;
             YLBL = 10;
+            Nieuwekleur = 1;
 
-            foreach (Product p in lijst_NietActiveroducten)
+            foreach (Besteld_product p in lijst_NietActiveroducten)
             {
-                KlaarKnoppen(ref teller, ref y, yToe, ref YLBL, p);
+                KlaarKnoppen(ref Nieuwekleur, ref NieuweTafel, ref teller, ref y, yToe, ref YLBL, p);
             }
 
-            foreach (Product p in Lijst_ActiveProducten)
+
+            teller = 1;
+            y = 0;
+            yToe = 40;
+            YLBL = 10;
+            NieuweTafel = 0;
+            Nieuwekleur = 1;
+
+            foreach (Besteld_product p in Lijst_ActiveProducten)
             {
-                Maakknoppen(ref teller, ref y, yToe, ref YLBL, p);
+                Maakknoppen(ref Nieuwekleur, ref NieuweTafel, ref teller, ref y, yToe, ref YLBL, p);
             }
 
         }
 
-       
 
-        private void KlaarKnoppen(ref int teller, ref int y, int yToe, ref int YLBL, Product p)
+
+        private void KlaarKnoppen(ref int Nieuwekleur, ref int NieuweTafel, ref int teller, ref int y, int yToe, ref int YLBL, Besteld_product p)
         {
-            y += yToe;
-            Button buttonKlaar = new System.Windows.Forms.Button();
-            buttonKlaar.Location = new System.Drawing.Point(47, y);
-            buttonKlaar.Name = "buttonklaar" + teller;
-            buttonKlaar.Size = new System.Drawing.Size(745, 34);
-            buttonKlaar.TabIndex = 101;
-            buttonKlaar.UseVisualStyleBackColor = true;
-
             Label BestelKlaar_lbl = new System.Windows.Forms.Label();
             BestelKlaar_lbl.AutoSize = true;
-            BestelKlaar_lbl.Location = new System.Drawing.Point(90, YLBL += yToe);
+            BestelKlaar_lbl.Location = new System.Drawing.Point(90 + 70, YLBL += yToe);
             BestelKlaar_lbl.Name = "Bestelklaar_lbl" + teller;
             BestelKlaar_lbl.Size = new System.Drawing.Size(35, 13);
             BestelKlaar_lbl.TabIndex = 22;
-            BestelKlaar_lbl.Text = p.ToString();
+            BestelKlaar_lbl.Text = (p.Omschrijving).ToString();
+            BestelKlaar_lbl.BackColor = System.Drawing.Color.Gainsboro;
 
-            Button NietKlaar_btn = new System.Windows.Forms.Button();
-            NietKlaar_btn.BackColor = System.Drawing.Color.Red;
-            NietKlaar_btn.Location = new System.Drawing.Point(798, y);
-            NietKlaar_btn.Name = "nietKlaar_btn" + teller;
-            NietKlaar_btn.Size = new System.Drawing.Size(78, 34);
-            NietKlaar_btn.TabIndex = 32;
-            NietKlaar_btn.Text = "Niet Klaar";
-            NietKlaar_btn.UseVisualStyleBackColor = true;
 
-            
+            y += yToe;
+            Button buttonKlaar = new System.Windows.Forms.Button();
+            buttonKlaar.Location = new System.Drawing.Point(47 + 70, y);
+            buttonKlaar.Name = "buttonklaar" + teller;
+            buttonKlaar.Size = new System.Drawing.Size(645 + 30, 34);
+            buttonKlaar.TabIndex = 101;
+            buttonKlaar.UseVisualStyleBackColor = true;
 
-            if (teller % 2 == 0)
+            Label Opmerking_lbl = new System.Windows.Forms.Label();
+            Opmerking_lbl.AutoSize = true;
+            Opmerking_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            Opmerking_lbl.Location = new System.Drawing.Point(90 + 100, YLBL + 10);
+            Opmerking_lbl.ForeColor = System.Drawing.Color.Red;
+            Opmerking_lbl.Name = "Opmerking_lbl" + teller;
+            Opmerking_lbl.Size = new System.Drawing.Size(35, 13);
+            Opmerking_lbl.TabIndex = 22;
+            Opmerking_lbl.Text = p.Opmerking.ToString();
+
+
+
+            if (NieuweTafel != p.Tafel_nummer)
             {
+                Label Tafelnummer_lbl = new System.Windows.Forms.Label();
+                Tafelnummer_lbl.AutoSize = true;
+                Tafelnummer_lbl.Location = new System.Drawing.Point(85, y + 10);
+                Tafelnummer_lbl.Name = "Bestel_lbl" + teller;
+                Tafelnummer_lbl.Size = new System.Drawing.Size(40, 34);
+                Tafelnummer_lbl.TabIndex = 22;
+                Tafelnummer_lbl.Text = p.Tafel_nummer.ToString();
+                Geschiedenis.Controls.Add(Tafelnummer_lbl);
+                Tafelnummer_lbl.BackColor = System.Drawing.Color.Gainsboro;
 
+                Button Tafel_btn = new System.Windows.Forms.Button();
+                Tafel_btn.Location = new System.Drawing.Point(70, y);
+                Tafel_btn.Name = "button" + teller;
+                Tafel_btn.Size = new System.Drawing.Size(40, 34);
+                Tafel_btn.TabIndex = 101;
+                Tafel_btn.UseVisualStyleBackColor = true;
+                Geschiedenis.Controls.Add(Tafel_btn);
+                Opmerking_lbl.BackColor = System.Drawing.Color.Gainsboro;
+
+
+                Button NietKlaar_btn = new System.Windows.Forms.Button();
+                NietKlaar_btn.BackColor = System.Drawing.Color.Red;
+                NietKlaar_btn.Location = new System.Drawing.Point(798, y);
+                NietKlaar_btn.Name = "nietKlaar_btn" + teller;
+                NietKlaar_btn.Size = new System.Drawing.Size(78, 34);
+                NietKlaar_btn.TabIndex = 32;
+                NietKlaar_btn.Text = "Niet Klaar";
+                NietKlaar_btn.UseVisualStyleBackColor = true;
+                NietKlaar_btn.Click += new EventHandler(NietKlaarClick);
+                NietKlaar_btn.Tag = p;
+                Geschiedenis.Controls.Add(NietKlaar_btn);
+
+                if (Nieuwekleur % 2 == 0)
+                {
+                    NietKlaar_btn.BackColor = System.Drawing.Color.DarkGray;
+                    Tafel_btn.BackColor = System.Drawing.Color.DarkGray;
+                    //opmerking.BackColor = System.Drawing.Color.Silver;
+                    Tafelnummer_lbl.BackColor = System.Drawing.Color.DarkGray;
+                    Opmerking_lbl.BackColor = System.Drawing.Color.DarkGray;
+                }
+
+                Nieuwekleur++;
+            }
+
+            NieuweTafel = p.Tafel_nummer;
+
+
+
+
+
+
+            if (Nieuwekleur % 2 != 0)
+            {
+                BestelKlaar_lbl.BackColor = System.Drawing.Color.DarkGray;
                 buttonKlaar.BackColor = System.Drawing.Color.DarkGray;
             }
 
+
             teller++;
             Geschiedenis.Controls.Add(BestelKlaar_lbl);
-            Geschiedenis.Controls.Add(NietKlaar_btn);
+
+            if (p.Opmerking != " ")
+            {
+
+
+                Geschiedenis.Controls.Add(Opmerking_lbl);
+            }
+
+
             Geschiedenis.Controls.Add(buttonKlaar);
         }
 
-        public void Maakknoppen(ref int teller, ref int y, int yToe, ref int YLBL, Product p)
+
+        /////////////////////////////////////////////////////////////////////////////////// Afscheiding
+
+        private void Maakknoppen(ref int Nieuwekleur, ref int NieuweTafel, ref int teller, ref int y, int yToe, ref int YLBL, Besteld_product p)
         {
             y += yToe;
             Button button = new System.Windows.Forms.Button();
-            button.Location = new System.Drawing.Point(47, y);
+            button.Location = new System.Drawing.Point(47 + 70, y);
             button.Name = "button" + teller;
-            button.Size = new System.Drawing.Size(745, 34);
+            button.Size = new System.Drawing.Size(645 + 30, 34);
             button.TabIndex = 101;
             button.UseVisualStyleBackColor = true;
 
             Label Bestel_lbl = new System.Windows.Forms.Label();
             Bestel_lbl.AutoSize = true;
-            Bestel_lbl.Location = new System.Drawing.Point(90, YLBL += yToe);
+            Bestel_lbl.Location = new System.Drawing.Point(90 + 65, YLBL += yToe);
             Bestel_lbl.Name = "Bestel_lbl" + teller;
             Bestel_lbl.Size = new System.Drawing.Size(35, 13);
             Bestel_lbl.TabIndex = 22;
-            Bestel_lbl.Text = p.ToString();
+            Bestel_lbl.Text = p.Omschrijving.ToString();
+            Bestel_lbl.BackColor = System.Drawing.Color.Gainsboro;
 
-            Button Klaar_btn = new System.Windows.Forms.Button();
-            Klaar_btn.Location = new System.Drawing.Point(798, y);
-            Klaar_btn.Name = "Klaar_btn" + teller;
-            Klaar_btn.Size = new System.Drawing.Size(78, 34);
-            Klaar_btn.TabIndex = 32;
-            Klaar_btn.Text = "Klaar";
-            Klaar_btn.UseVisualStyleBackColor = true;
-            Klaar_btn.Click += new EventHandler(handleClick);
-            Klaar_btn.Tag = p;
+            Label Opmerking_lbl = new System.Windows.Forms.Label();
+            Opmerking_lbl.AutoSize = true;
+            Opmerking_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            Opmerking_lbl.Location = new System.Drawing.Point(90 + 100, YLBL + 10);
+            Opmerking_lbl.ForeColor = System.Drawing.Color.Red;
+            Opmerking_lbl.Name = "Opmerking_lbl" + teller;
+            Opmerking_lbl.Size = new System.Drawing.Size(35, 13);
+            Opmerking_lbl.TabIndex = 22;
+            Opmerking_lbl.Text = p.Opmerking.ToString();
+            Opmerking_lbl.BackColor = System.Drawing.Color.Gainsboro;
 
-            
-
-            if (teller % 2 == 0)
+            if (NieuweTafel != p.Tafel_nummer)
             {
-                Klaar_btn.BackColor = System.Drawing.Color.DarkGray;
+                Label Tafelnummer_lbl = new System.Windows.Forms.Label();
+                Tafelnummer_lbl.AutoSize = true;
+                Tafelnummer_lbl.Location = new System.Drawing.Point(85, y + 10);
+                Tafelnummer_lbl.Name = "Bestel_lbl" + teller;
+                Tafelnummer_lbl.Size = new System.Drawing.Size(40, 34);
+                Tafelnummer_lbl.TabIndex = 22;
+                Tafelnummer_lbl.Text = p.Tafel_nummer.ToString();
+                Tafelnummer_lbl.BackColor = System.Drawing.Color.Gainsboro;
+                BarNu.Controls.Add(Tafelnummer_lbl);
+
+                Button Tafel_btn = new System.Windows.Forms.Button();
+                Tafel_btn.Location = new System.Drawing.Point(70, y);
+                Tafel_btn.Name = "button" + teller;
+                Tafel_btn.Size = new System.Drawing.Size(40, 34);
+                Tafel_btn.TabIndex = 101;
+                Tafel_btn.UseVisualStyleBackColor = true;
+                BarNu.Controls.Add(Tafel_btn);
+
+
+                Button Klaar_btn = new System.Windows.Forms.Button();
+                Klaar_btn.Location = new System.Drawing.Point(798, y);
+                Klaar_btn.Name = "Klaar_btn" + teller;
+                Klaar_btn.Size = new System.Drawing.Size(78, 34);
+                Klaar_btn.TabIndex = 32;
+                Klaar_btn.Text = "Klaar";
+                Klaar_btn.UseVisualStyleBackColor = true;
+                Klaar_btn.Click += new EventHandler(KlaarClick);
+                Klaar_btn.Tag = p;
+                BarNu.Controls.Add(Klaar_btn);
+
+                if (Nieuwekleur % 2 == 0)
+                {
+                    Klaar_btn.BackColor = System.Drawing.Color.DarkGray;
+                    Tafel_btn.BackColor = System.Drawing.Color.DarkGray;
+                    Tafelnummer_lbl.BackColor = System.Drawing.Color.DarkGray;
+                    Opmerking_lbl.BackColor = System.Drawing.Color.DarkGray;
+                }
+
+                Nieuwekleur++;
+            }
+
+
+
+            NieuweTafel = p.Tafel_nummer;
+
+
+
+
+            if (Nieuwekleur % 2 != 0)
+            {
+                Bestel_lbl.BackColor = System.Drawing.Color.DarkGray;
                 button.BackColor = System.Drawing.Color.DarkGray;
             }
 
+
+
+
             teller++;
+
+
             BarNu.Controls.Add(Bestel_lbl);
-            BarNu.Controls.Add(Klaar_btn);
+
+            if (p.Opmerking != " ")
+            {
+
+
+                BarNu.Controls.Add(Opmerking_lbl);
+            }
+
+
             BarNu.Controls.Add(button);
+
+
+
         }
 
-        private void handleClick(object sender, EventArgs e)
+        private void KlaarClick(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            KeukenDAO keukenDAO = new KeukenDAO();
             Button button = (Button)sender;
-            int bestellingID = (int)button.Tag;
+            Besteld_product product = (Besteld_product)button.Tag;
+            keukenDAO.KeukenBestellingKlaar(product.Tafel_nummer);
+            // OverzichtKeuken.TabPages.Clear();
+
+
+            ActiveForm.Refresh();
+        }
+
+        private void NietKlaarClick(object sender, EventArgs e)
+        {
+            KeukenDAO keukenDAO = new KeukenDAO();
+            Button button = (Button)sender;
+            Besteld_product product = (Besteld_product)button.Tag;
+            keukenDAO.KeukenBestellingNIETKlaar(product.Tafel_nummer);
         }
 
         private void button42_Click(object sender, EventArgs e)
@@ -158,6 +318,11 @@ namespace Classes_Project
         }
 
         private void KeukenOverzicht_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opmerking_Click(object sender, EventArgs e)
         {
 
         }
